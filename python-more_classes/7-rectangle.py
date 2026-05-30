c252f3f83f5625cd3c12f1644a6152076a93f17a
Rebaone Seilane <rebaoneseilane@gmail.com>
2026-05-30 17:04:31 +0000

    Task2

---
+++ python-more_classes/7-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""Module that defines Rectangle with custom print symbol."""
+
+
+class Rectangle:
+    """Rectangle class with customizable print symbol."""
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Initialize rectangle."""
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Get width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set width."""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height."""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return area."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return perimeter."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return printable rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        symbol = str(self.print_symbol)
+        return "\n".join(symbol * self.__width for _ in range(self.__height))
+
+    def __repr__(self):
+        """Return eval representation."""
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        """Handle deletion."""
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
