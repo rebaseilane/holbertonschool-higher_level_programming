9ed74aabbd045fc57f5f390c3d3033a94106af6f
Rebaone Seilane <rebaoneseilane@gmail.com>
2026-06-01 08:27:33 +0000

    Task 1.2

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- Creates table unique_id with UNIQUE constraint on id
+
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- Lists cities of California using subquery
+
+SELECT id, name
+FROM cities
+WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
+ORDER BY id ASC;
