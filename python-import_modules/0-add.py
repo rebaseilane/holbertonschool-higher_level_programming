5f370327caac7092a5716c9f64cb6d0743a2065d
Rebaone Seilane <rebaoneseilane@gmail.com>
2026-06-02 15:46:34 +0200

    Make script executable

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""
-Imports a function from another file and prints the result of addition.
-"""
+
+from add_0 import add
 
 if __name__ == "__main__":
-    add = __import__("add_0").add
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
