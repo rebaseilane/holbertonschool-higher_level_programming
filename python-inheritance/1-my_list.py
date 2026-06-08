8288e797c440edbfa8674d426b9a39bbf3ff2647
Rebaone Seilane <rebaoneseilane@gmail.com>
2026-06-08 11:39:25 +0200

    Task Fixingv1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,6 +3,8 @@
 
 
 class MyList(list):
+    """Class that inherits from list."""
 
     def print_sorted(self):
+        """Print the list in ascending sorted order."""
         print(sorted(self))
